f156d65673984d5f043ce5b831d30bc0d2b5754c
yuyunfrancis <francisberi@yahoo.com>
2022-03-02 05:37:38 -0800

    components setup and general css

---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./contact.css";
+
+const Contact = () => {
+  return <div></div>;
+};
+
+export default Contact;
---
+++ src/components/testimonials/testimonials.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./testimonials.css";
+
+const testimonials = () => {
+  return <div></div>;
+};
+
+export default testimonials;
