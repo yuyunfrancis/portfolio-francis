9b5e5c46bbcdcdc74171a8fa705bb9f68c7266c4
yuyunfrancis <francisberi@yahoo.com>
2022-03-16 01:28:21 -0700

    #experience section started

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,6 +1,48 @@
 import React from "react";
 import "./experience.css";
 
+import { BsPatchCheckFill } from "react-icons/bs";
+
+const dataExperience = [
+  {
+    id: 1,
+    experienceName: "HTML",
+    experienceLevel: "Experienced",
+    icon: BsPatchCheckFill,
+  },
+
+  {
+    id: 2,
+    experienceName: "CSS",
+    experienceLevel: "Intermediate",
+    icon: BsPatchCheckFill,
+  },
+  {
+    id: 3,
+    experienceName: "JavaScript",
+    experienceLevel: "Intermediate",
+    icon: BsPatchCheckFill,
+  },
+  {
+    id: 4,
+    experienceName: "React Js",
+    experienceLevel: "Intermediate",
+    icon: BsPatchCheckFill,
+  },
+  {
+    id: 5,
+    experienceName: "Tailwind",
+    experienceLevel: "Intermediate",
+    icon: BsPatchCheckFill,
+  },
+  // {
+  //   id: 6,
+  //   experienceName: "",
+  //   experienceLevel: "Experienced",
+  //   icon: BsPatchCheckFill,
+  // },
+];
+
 const Experience = () => {
   return (
     <section id="experience">
@@ -8,7 +50,20 @@ const Experience = () => {
       <h2>My Experience</h2>
 
       <div className="container experience__container">
-        <div className="experience__frontend"></div>
+        <div className="experience__frontend">
+          <h3>Fronend Development</h3>
+          <div className="experience__content">
+            {dataExperience.map((item) => {
+              return (
+                <article className="experience__details" key={item.id}>
+                  <item.icon />
+                  <h4>{item.experienceName}</h4>
+                  <small className="text-light">{item.experienceLevel}</small>
+                </article>
+              );
+            })}
+          </div>
+        </div>
 
         <div className="experience__uiux-designer"></div>
       </div>
